df95ee9c9048b22cd48ff7da11497177dbc52f88
awhb <awhangbin@gmail.com>
2024-06-28 03:00:04 +0800

    refactor users controller

---
+++ controllers/usersController.go
@@ -0,0 +1,125 @@
+package controllers
+
+import (
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/awhb/gitgood-backend/initialisers"
+	"github.com/awhb/gitgood-backend/models"
+
+	"github.com/gin-gonic/gin"
+	"github.com/golang-jwt/jwt/v4"
+	"golang.org/x/crypto/bcrypt"
+)
+
+func Signup(c *gin.Context) {
+	// Get data off request body
+	var body struct {
+		Email string
+		Password string
+	}
+
+	if c.Bind(&body) != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": "Failed to read body",
+		})
+		return
+	}
+
+	// Hash password
+	hash, err :=bcrypt.GenerateFromPassword([]byte(body.Password), 10)
+
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": "Failed to hash password",
+		})
+		return
+	}
+
+	// Create user
+	user := models.User{Email: body.Email, Password: string(hash)}
+	result := initialisers.DB.Create(&user)
+
+	if result.Error != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": "Failed to create user",
+		})
+		return
+	}
+
+	// Respond
+	c.JSON(http.StatusOK, gin.H{
+		"message": "Created user",
+	})
+}
+
+func Login(c *gin.Context) {
+	// Get data off request body
+	var body struct {
+		Email string
+		Password string
+	}
+
+	if c.Bind(&body) != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": "Failed to read body",
+		})
+		return
+	}
+
+	// Check if user exists
+	var user models.User
+	initialisers.DB.First(&user, "email = ?", body.Email)
+
+	if user.ID == 0 {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": "Invalid email or password",
+		})
+		return
+	}
+
+	// Compare sent in password with saved user hash
+	err := bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(body.Password))
+
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": "Invalid email or password",
+		})
+		return
+	}
+
+	// Generate a JWT token
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"subject": user.ID,
+		"expires": time.Now().Add(time.Hour * 24 * 30).Unix(),
+	})
+
+	// Sign and get the complete encoded token as a string
+	tokenString, err := token.SignedString([]byte(os.Getenv("JWT_SECRET")))
+
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": "Failed to create token",
+		})
+		return
+	}
+
+	// Success
+	c.SetSameSite(http.SameSiteLaxMode)
+	c.SetCookie("Authorisation", tokenString, 3600*24*30, "", "", os.Getenv("GIN_MODE") == "release", true)
+
+	c.JSON(http.StatusOK, gin.H{
+		"token": tokenString,
+	})
+}
+
+func Validate(c *gin.Context) {
+	user, _ := c.Get("user")
+
+	c.JSON(http.StatusOK, gin.H{
+		"message": "You are logged in!",
+		"user":    user,
+	})
+}
+
