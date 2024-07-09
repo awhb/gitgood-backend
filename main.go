bc73680bf987a627f8c64a18071f1d6e8e69d1d9
awhb <awhangbin@gmail.com>
2024-07-09 22:59:39 +0800

    update port

--- main.go
+++ main.go
@@ -22,7 +22,7 @@ func main() {
     r := gin.Default()
 
     r.Use(cors.New(cors.Config{
-        AllowOrigins:     []string{"http://localhost:5173"}, // Update with your frontend URL
+        AllowOrigins:     []string{"http://localhost:3000"}, // Update with your frontend URL
         AllowMethods:     []string{"GET", "POST", "PUT", "DELETE"},
         AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
         ExposeHeaders:    []string{"Content-Length"},
