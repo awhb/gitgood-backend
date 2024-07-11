f13f4acfbfaa7709dfa87abf520bbd651a214a22
awhb <awhangbin@gmail.com>
2024-07-12 00:07:46 +0800

    handle error middleware

--- middleware/requireAuth.go
+++ middleware/requireAuth.go
@@ -17,18 +17,26 @@ func RequireAuth(c *gin.Context) {
 	// get authorization header
 	tokenString, err := c.Cookie("Authorization")
 
-	if err != nil {
-		c.AbortWithStatus(http.StatusUnauthorized)
+	if err != nil || tokenString == "" {
+		c.JSON(http.StatusUnauthorized, gin.H{"error": "No token provided"})
+		c.Abort()
+		return
 	}
 
 	// Decode/validate cookie
-	token, _ := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
 		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
 			return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
 		}
 
 		return []byte(os.Getenv("JWT_SECRET")), nil
 	})
+
+	if err != nil {
+		c.JSON(http.StatusUnauthorized, gin.H{"error": err.Error()})
+		c.Abort()
+		return
+	}
 
 	if claims, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
 		// Check the expiry
