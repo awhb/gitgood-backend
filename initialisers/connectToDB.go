efb978e8955437d0fef8ba13b1a3ed8d0d9c59aa
awhb <awhangbin@gmail.com>
2024-07-11 15:07:25 +0800

    remove supabase

--- initialisers/connectToDB.go
+++ initialisers/connectToDB.go
@@ -3,7 +3,6 @@ package initialisers
 import (
 	"log"
 	"os"
-
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
@@ -12,13 +11,10 @@ var DB *gorm.DB
 
 func ConnectToDB() {
 	var err error
-	dsn := os.Getenv("DB_URL")
-	
-	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
 
 	// connect to local database on port 5432
-	// dsn := "host=localhost user=postgres password=postgres dbname=postgres port=5432 sslmode=disable"
-	// DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	dsn := os.Getenv("DB_URL")
+	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
 
 	if err != nil {
 		log.Fatal("Failed to connect to database")
